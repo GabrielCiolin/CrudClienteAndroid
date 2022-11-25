24d0b588fa9a317be225e7c1bef0fe5d258a4b3b
Gabriel C. Fasolo <gabrielciolinfasolo@hotmail.com>
2022-11-25 00:43:08 -0300

    ADD Cadastro de Pessoas

--- app/src/main/java/com/example/trabalhocrud/SecondFragment.kt
+++ app/src/main/java/com/example/trabalhocrud/SecondFragment.kt
@@ -1,6 +1,7 @@
 package com.example.trabalhocrud
 
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +11,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.room.Room
 import com.example.trabalhocrud.Model.Pessoa
 import com.example.trabalhocrud.databinding.FragmentSecondBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -26,25 +28,7 @@ class SecondFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val applicationContext = activity?.applicationContext;
-        Thread {
-            val db = Room.databaseBuilder(
-                applicationContext!!,
-                AppDatabase::class.java, "banco"
-            ).build()
 
-            var fulano: Pessoa = Pessoa(
-                5,
-                "Gabriel Ciolin",
-                "gabriel@hotmail.com",
-                "44999981716"
-            )
-
-            val pessoaDao = db.pessoaDao()
-//            pessoaDao.insert(fulano)
-            val pessoas = pessoaDao.getAll()
-            Log.e("Pessoa ",pessoas.toString())
-        }.start()
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -53,8 +37,38 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnCadastrar.setOnClickListener {
+            val id = binding.idPessoa.text.toString()
+            val novoId= Integer.parseInt(id)
+            val nome = binding.nome.text.toString()
+            val email = binding.email.text.toString()
+            val celular = binding.celular.text.toString()
+            var fulano: Pessoa = Pessoa(
+                novoId,
+                nome,
+                email,
+                celular
+            )
+
+            Thread {
+                val applicationContext = activity?.applicationContext;
+                val db = Room.databaseBuilder(
+                    applicationContext!!,
+                    AppDatabase::class.java, "banco"
+                ).build()
+
+                val pessoaDao = db.pessoaDao()
+                pessoaDao.insert(fulano)
+                val pessoa = pessoaDao.get(novoId)
+                Log.e("Pessoa ",pessoa.toString())
+            }.start()
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+
+        }
+
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+
         }
     }
 
